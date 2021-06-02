7fa826f63e1e627b0beae346cdd79a8f0548fdaf
Pranil Kharche <pranilkharche@gmail.com>
2021-06-02 16:26:56 +0530

    Globaled UserType + bug fixes

--- src/plugins/AuthContext.js
+++ src/plugins/AuthContext.js
@@ -10,7 +10,9 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
-  const signup = (email, password, fullName, signType) => {
+  const [userType, setUserType] = useState();
+
+  const signup = async (email, password, fullName, signType) => {
     let promise = new Promise(function (resolve, reject) {
       auth
         .createUserWithEmailAndPassword(email, password)
@@ -18,8 +20,12 @@ export function AuthProvider({ children }) {
           ref.user.updateProfile({
             displayName: fullName,
           });
-          afterSignUp(ref.user.uid, signType, email, password, fullName);
-          resolve(ref);
+          afterSignUp(ref.user.uid, signType, email, password, fullName).then(
+            (data) => {
+              console.log(data);
+              resolve(ref);
+            }
+          );
         })
         .catch((error) => reject(error));
     });
@@ -60,22 +66,59 @@ export function AuthProvider({ children }) {
     return promise;
   };
 
+  const checkUserType = async (user) => {
+    if (user) {
+      await user
+        .getIdTokenResult()
+        .then((idTokenResult) => {
+          console.log(idTokenResult);
+          // Confirm the user is an Admin.
+          if (idTokenResult.claims.userType === "institute") {
+            console.log("institute");
+            setUserType("institute");
+            setCurrentUser(user);
+            setLoading(false);
+            // Show admin UI.
+          } else if (idTokenResult.claims.userType === "student") {
+            setUserType("student");
+            setCurrentUser(user);
+            setLoading(false);
+            // Show regular user UI.
+            console.log("student");
+          } else {
+            setLoading(false);
+            console.log("User Signed In But No Claims inside token ID Result");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      setLoading(false);
+      console.log("NO User Found");
+    }
+  };
+
   useEffect(() => {
-    const unsubscribe = () =>
-      auth.onAuthStateChanged((user) => {
-        setCurrentUser(user);
-        setLoading(false);
+    console.log("Use Effect");
+    const unsubscribe = async () =>
+      auth.onAuthStateChanged(async (user) => {
+        console.log("onAuthStateChanged");
+        checkUserType(user);
       });
     return unsubscribe();
-  }, [currentUser]);
+  }, []);
 
   const value = {
+    userType,
     currentUser,
     signup,
     signin,
     signout,
     passwordReset,
   };
+
+  <auth className="currentUser"></auth>;
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
