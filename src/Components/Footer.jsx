25be74f06a1b8a49e36098129c75f6b54634791c
Pranil Kharche <pranilkharche@gmail.com>
2021-06-06 23:47:23 +0530

    Packages + Footer Fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = (props) => {
   return (
     <>
       <div className="footer">
-        <nav className="navbar fixed-bottom navbar-light bg-light">
+        <nav className="navba navbar-light bg-light">
           <p className="m-2 text-muted">Copyright (c) MetricGrade 2021</p>
           <div className="d-flex">
             <p className="m-2 text-muted">All Rights Reserved.</p>
